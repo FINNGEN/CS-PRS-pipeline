a64ca8dcc621ddaf3078009aa5b63ff0e8bd7682
pete <pietro.dellabriottaparolo@gmail.com>
2022-10-06 09:50:08 +0300

    changes

--- scripts/localize_release.sh
+++ scripts/localize_release.sh
@@ -13,11 +13,11 @@ mkdir -p $OUT_PATH/munged_rsid/
 
 # SCORES & LOGS
 echo "LOGS"
-wc -l <  <( gsutil ls gs://$CROMWELL_NAME/prs_cs/$CROMWELL_ID/call-scores/**/finngen*log )
+wc -l <  <(gsutil ls gs://$CROMWELL_NAME/prs_cs/$CROMWELL_ID/call-scores/**/finngen*log)
 gsutil -m cp  gs://$CROMWELL_NAME/prs_cs/$CROMWELL_ID/call-scores/**/finngen*log $OUT_PATH/release/documentation/ > /dev/null 2>&1
 
 echo "SCORES"
-wc -l <  ( gsutil ls gs://$CROMWELL_NAME/prs_cs/$CROMWELL_ID/call-scores/**/finngen*sscore)
+wc -l <  <( gsutil ls gs://$CROMWELL_NAME/prs_cs/$CROMWELL_ID/call-scores/**/finngen*sscore)
 gsutil -m cp         gs://$CROMWELL_NAME/prs_cs/$CROMWELL_ID/call-scores/**/finngen*sscore $OUT_PATH/release/data/ > /dev/null 2>&1
 
 # WEIGHTS
